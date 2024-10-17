a9902e461e6885b259b79c71ac498542f1cddd86
Priyanka Sinha <sngpriyanka445@gmail.com>
2024-10-17 16:12:13 +0545

    remote instruction added

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 // git status---to check the project status
 // git help or git -h
 // git add filename -- to add a file
-// git add . -- add all files\
+// git add . // add all files
 // git commit -m "message"
+// git remote -v // view the remote origin
