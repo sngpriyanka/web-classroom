9bdd43716174485e82e09c70507bd00663799d0b
Priyanka Sinha <sngpriyanka445@gmail.com>
2024-10-17 15:37:35 +0545

    file added

---
+++ index.js
@@ -0,0 +1,6 @@
+// git init---to initialize the project
+// git status---to check the project status
+// git help or git -h
+// git add filename -- to add a file
+// git add . -- add all files\
+// git commit -m "message"
