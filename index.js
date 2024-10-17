ad998b01a1ca01c0a861f004eb4ea63e84e1af45
Priyanka Sinha <sngpriyanka445@gmail.com>
2024-10-17 16:58:39 +0545

    index code added

--- index.js
+++ index.js
@@ -5,3 +5,9 @@
 // git add . // add all files
 // git commit -m "message"
 // git remote -v // view the remote origin
+// git log -- show commit history
+// git checkout -b new branch name // create new branch
+// git branch // to check branch
+// git fetch origin // pulls all branch from server
+// git branch -a // show remote branch too
+// git checkout branch name // change the branch
